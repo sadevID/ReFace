b44be08d001e777b70c0b6218e4c3443c5c728ac
SA DevLabs <62686948+sadevID@users.noreply.github.com>
2023-11-13 16:54:56 +0700

    Update metadata.py

--- facefusion/metadata.py
+++ facefusion/metadata.py
@@ -1,11 +1,11 @@
 METADATA =\
 {
-	'name': 'FaceFusion',
+	'name': 'ReFace',
 	'description': 'Next generation face swapper and enhancer',
-	'version': '1.3.1',
+	'version': '1.0',
 	'license': 'MIT',
-	'author': 'Henry Ruhs',
-	'url': 'https://facefusion.io'
+	'author': 'SA DevLabs',
+	'url': 'https://dev.samedia.biz.id'
 }
 
 
